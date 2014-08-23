c9f0d1359e8b587d716519a0ed75a80725d0cbd4
Tim Brooks <tim.brooks@staples.com>
2014-08-22 22:13:27 -0700

    Add code to prep the data

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,14 @@
 data(mtcars)
 library(ggplot2)
+library(knitr)
 
 plot <- function() {
   p <- ggplot(mtcars, aes(wt, mpg))  + geom_point(aes(colour = factor(am)))
   p
+}
+
+prep_data <- function() {
+  data <- mtcars
+  ata$am <- as.factor(data$am)
+  data
 }
