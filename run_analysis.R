6a349997c68ca08994bb1d4c992d8318ed22d763
Tim Brooks <tim.brooks@staples.com>
2014-08-23 17:19:36 -0700

    Updating data to use a boolean variable

--- run_analysis.R
+++ run_analysis.R
@@ -2,13 +2,14 @@ data(mtcars)
 library(ggplot2)
 library(knitr)
 
-plot <- function() {
-  p <- ggplot(mtcars, aes(wt, mpg))  + geom_point(aes(colour = factor(am)))
+plot <- function(data) {
+  p <- ggplot(data, aes(wt, mpg))  + geom_point(aes(colour = factor(automatic)))
   p
 }
 
 prep_data <- function() {
   data <- mtcars
   data$am <- as.factor(data$am)
+  data$automatic <- data$am == "0"
   data
 }
